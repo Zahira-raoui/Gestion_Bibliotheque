58b4dfb3d4747712824def4e9dda998121f4669d
User-sam21 <samirabelmabkhoute@gmail.com>
2024-10-27 02:42:06 +0100

    views

--- Project/views.py
+++ Project/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import redirect, render
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views import generic
+from .models import User, Book, Chat, DeleteRequest, Feedback
+from django.contrib import messages
+from django.db.models import Sum
+from django.views.generic import CreateView, DetailView, DeleteView, UpdateView, ListView
+from . import models
+import operator
+import itertools
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth import authenticate, logout
+from django.contrib import auth, messages
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
+
+
+
+# Shared Views
+def login_form(request):
+	return render(request, 'bookstore/login.html')
+
